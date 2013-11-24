89b3d9a5244986fa4a76d617af569c7071b882bd
Rashaad Ramdeen <rashaad.ramdeen@gmail.com>
2013-11-23 22:35:21 -0500

    fixed expand bug

--- js/responsive-menu.js
+++ js/responsive-menu.js
@@ -2,21 +2,23 @@ $(function(){
 	var nav = $('nav ul');
 	var btn = $("#responsive-btn");
 
-	var resUpDownState = 0; // responsive mode, 0 = up , 1 = down
-
 	btn.click(function(){
 		if(window.innerWidth <= 768) {
 			if(nav.css('display') == 'none'){
+				console.log("down");
 				nav.slideDown('fast');
 			}else{
+				console.log("up");
 				nav.slideUp('fast');
 			}
 		}
 	});
 
 	$(window).resize(function(){
-		if($(window).width() > 768 && nav.css('display') == 'none'){
+		if($(window).width() >= 768){
 			nav.show();
+		}else{
+			nav.hide();
 		}
 	});
 
